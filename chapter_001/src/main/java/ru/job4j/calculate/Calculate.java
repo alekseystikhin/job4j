2578e5b21564b066f7f5c968b2cb0bd07fe3b1a9
aleksey stikhin <stikhinsky@gmail.com>
2019-10-11 21:19:36 +0300

    JavaDoc. Code convention[#184973]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculate;
+
+/**
+ * Class Класс для вычисления арифметических операций + - * / ^.
+ *
+ * @author Aleksey Stikhin (stikhinsky@mail.ru)
+ * @version 1
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Конструктор, вывод строки в консоль.
+     *
+     * @param args - args
+     */
+    public static void main(String[] args) {
+        System.out.println("Calculate...");
+    }
+}
