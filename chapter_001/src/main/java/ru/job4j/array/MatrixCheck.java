d4f92ebaac225407c25eed8fb9e99b9b14d3e9ef
aleksey stikhin <stikhinsky@gmail.com>
2019-10-12 23:37:44 +0300

    Выигрышные комбинации в сокобан[#184986], исправления

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -6,59 +6,24 @@ public class MatrixCheck {
         for (int row = 0; row < board.length; row++) {
             for (int cell = 0; cell < board.length; cell++) {
                 char sign = board[row][cell];
-                System.out.print(sign);
                 if (sign == 'X') {
-                    boolean inRow = true;
-                    boolean inCell = true;
+                    int countXInCell = 0;
+                    int countXInRow = 0;
                     for (int i = 0; i < 5; i++) {
-                        if (board[row][i] != 'X') {
-                            inCell = false;
-                            break;
+                        if (board[row][i] == 'X') {
+                            countXInCell++;
+                        }
+                        if (board[i][cell] == 'X') {
+                            countXInRow++;
+                        }
+                        if ((countXInCell == board.length) || (countXInRow == board.length)) {
+                            result = true;
                         }
                     }
-                    for (int j = 0; j < 5; j++) {
-                        if (board[j][cell] != 'X') {
-                            inRow = false;
-                            break;
-                        }
-                    }
-                    result = inRow || inCell;
+                    return result;
                 }
             }
-            System.out.println();
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        char[][] hasWinVertical = {
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-        };
-        boolean win = isWin(hasWinVertical);
-        System.out.println("A board has a winner : " + win);
-        System.out.println();
-        char[][] hasWinHor = {
-                {'_', '_', '_', '_', '_'},
-                {'X', 'X', 'X', 'X', 'X'},
-                {'_', '_', '_', '_', '_'},
-                {'_', '_', '_', '_', '_'},
-                {'_', '_', '_', '_', '_'},
-        };
-        boolean winHor = isWin(hasWinHor);
-        System.out.println("A board has a winner : " + winHor);
-        System.out.println();
-        char[][] notWin = {
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', 'X', '_', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-                {'_', '_', 'X', '_', '_'},
-        };
-        boolean lose = isWin(notWin);
-        System.out.println("A board has a winner : " + lose);
     }
 }
