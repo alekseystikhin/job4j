55ed705096772e2cd4cd813d689884846a69bcbe
aleksey stikhin <stikhinsky@gmail.com>
2019-10-13 18:20:17 +0300

    Выигрышные комбинации в сокобан[#184986], исправления

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,31 +4,22 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int row = 0; row < board.length; row++) {
-            for (int cell = 0; cell < board.length; cell++) {
-                char sign = board[row][cell];
-                if (sign == 'X') {
-                    int countXInCell = 0;
-                    int countXInRow = 0;
-                    for (int i = 0; i < 5; i++) {
-                        if (board[row][i] == 'X') {
-                            countXInCell++;
-                        }
-                        if ((countXInCell == board.length)) {
-                            result = true;
-                            return result;
-                        }
+            char sign = board[row][row];  // тут нужно проверять диагональ
+            if (sign == 'X') {
+                int countXInCell = 0;
+                int countXInRow = 0;
+                for (int cell = 0; cell < board.length; cell++) {
+                    if (board[row][cell] == 'X') {
+                        countXInCell++;
                     }
-                    for (int j = 0; j < 5; j++) {
-                        if (board[j][cell] == 'X') {
-                            countXInRow++;
-                        }
-                        if ((countXInRow == board.length)) {
-                            result = true;
-                            return result;
-                        }
+                    if (board[cell][row] == 'X') {
+                        countXInRow++;
                     }
-                    return result;
                 }
+                if (countXInCell == board.length || countXInRow == board.length) {
+                    result = true;
+                }
+                break;
             }
         }
         return result;
