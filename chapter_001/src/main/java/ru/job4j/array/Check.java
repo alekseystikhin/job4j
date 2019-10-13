fd07d212d60716380deee220f42d90b656e9a52a
aleksey stikhin <stikhinsky@gmail.com>
2019-10-13 10:23:48 +0300

    Массив заполнен true или false[#184985], исправления

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -6,8 +6,8 @@ public class Check {
         for (int i = 1; i < data.length; i++) {
             if (data[i] != data[0]) {
                 result = false;
+                break;
             }
-            break;
         }
         return result;
     }
