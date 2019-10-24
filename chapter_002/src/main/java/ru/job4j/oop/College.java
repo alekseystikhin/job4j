a9a6af8aca35dd091916009a2a3262aee503b201
aleksey stikhin <stikhinsky@gmail.com>
2019-10-24 23:45:33 +0300

    3.1. Приведение типов. Повышение типа и понижение типа.[#185090], исправление

--- chapter_002/src/main/java/ru/job4j/oop/College.java
+++ chapter_002/src/main/java/ru/job4j/oop/College.java
@@ -4,7 +4,7 @@ public class College {
 
     public static void main(String[] args) {
         Freshman freshman = new Freshman();
-        Student student = (Student) freshman;
-        Object object = (Object) student;
+        Student student = freshman;
+        Object object = student;
     }
 }
