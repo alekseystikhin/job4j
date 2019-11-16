5c308dfeadffbe664e33b5ba7bae29115ff832b0
aleksey stikhin <stikhinsky@gmail.com>
2019-11-16 19:21:09 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API.[#184932]

--- chapter_003/src/main/java/com/Bank.java
+++ chapter_003/src/main/java/com/Bank.java
@@ -1,6 +1,7 @@
 package com;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Bank {
 
@@ -24,38 +25,31 @@ public class Bank {
 
     public List<Account> getUserAccounts(String passport) {
         List<Account> list = new ArrayList<>();
-        for (User key : users.keySet()) {
-            if (key.getPassport().equals(passport)) {
-                list = this.users.get(key);
-            }
-        }
+        users.keySet().stream()
+                .filter(user -> user.getPassport().equals(passport))
+                .forEach(user -> list.addAll(this.users.get(user)));
         return list;
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport, String dstRequisite, double amount) {
-        boolean src = false;
-        boolean dst = false;
         boolean transfer = false;
         List<Account> srcList = getUserAccounts(srcPassport);
         List<Account> dstList = getUserAccounts(destPassport);
 
-        for (Account account : srcList) {
-            if (account.getRequisites().equals(srcRequisite)) {
-                src = true;
-                srcList = Arrays.asList(account);
-                break;
+        srcList = srcList.stream()
+                .filter(account -> account.getRequisites().equals(srcRequisite))
+                .collect(Collectors.toList());
+
+        dstList = dstList.stream()
+                .filter(account -> account.getRequisites().equals(dstRequisite))
+                .collect(Collectors.toList());
+
+        if (!srcList.isEmpty() && !dstList.isEmpty()) {
+            if (srcList.get(0).transfer(dstList.get(0), amount)) {
+                transfer = true;
             }
         }
-        for (Account account : dstList) {
-            if (account.getRequisites().equals(dstRequisite)) {
-                dst = true;
-                dstList = Arrays.asList(account);
-                break;
-            }
-        }
-        if ((src && dst) && (srcList.get(0).transfer(dstList.get(0), amount))) {
-            transfer = true;
-        }
+
         return transfer;
     }
 }
