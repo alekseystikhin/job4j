08d56c823c2f4fd6e5406679b5fbe7210a858c54
aleksey stikhin <stikhinsky@gmail.com>
2020-02-04 21:59:18 +0300

    1. Создать модель User[#201676], исправления

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -1,12 +1,11 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
-import java.util.Objects;
 
 public class User {
-    String name;
-    int children;
-    Calendar birthday;
+    private String name;
+    private int children;
+    private Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
